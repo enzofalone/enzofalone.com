55324d6eec4ee7533d44844f72e218d906fa4e4a
Enzo Falone <enzofalone123@gmail.com>
2023-12-02 04:31:49 -0500

    remove deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://enzofalone.github.io',
-  base: '/enzofalone.com',
   integrations: []
 });
