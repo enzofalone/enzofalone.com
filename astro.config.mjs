395c1b2d1bbc9d27b47cb69d189448e86561022a
Enzo Falone <enzofalone123@gmail.com>
2023-12-02 04:18:52 -0500

    added base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://enzofalone.github.io',
-  // base: '/enzofalone.com',
+  base: '/enzofalone.com',
   integrations: []
 });
