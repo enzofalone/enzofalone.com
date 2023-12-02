ceefe0bd4407f0a47ff1fefa74db94758d88d69c
Enzo Falone <enzofalone123@gmail.com>
2023-12-02 03:51:02 -0500

    added icons, styles, projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://enzofalone.github.io',
+  // base: '/enzofalone.com',
   integrations: []
 });
